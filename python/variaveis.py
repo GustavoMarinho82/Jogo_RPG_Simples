922c653960f92fd33988db8bb81228b953493323
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2024-06-01 22:18:29 -0300

    Leitura das interações implementada
    Leitura das interações = Passar as interações do arquivo interações.txt pra variável interacoes em variaveis.py.
    E também outras alterações.

--- python/variaveis.py
+++ python/variaveis.py
@@ -2,10 +2,10 @@ from io import open
 
 
 # jogador = {"Local": [linha_do_castelo, coluna_do_castelo], ...}
-jogador = {"Local": [0, 0], "Vida": 100}
+jogador = {"Local": [0, 0], "Vida": 100, "Mana": 100, "Máximo de Mana": 100}
 
 
-# itens = {ID: {"Nome": x, "Descrição": y}, ...}  |  Tipos de itens: 0~1-> poções | 2~10-> diversos | 11~17-> anéis | 21~29-> armas | 31~37-> armaduras
+# itens = {id: {"Nome": x, "Descrição": y}, ...}  |  Tipos de itens: 0~1-> poções | 2~10-> diversos | 11~17-> anéis | 21~29-> armas | 31~37-> armaduras
 itens = {}
 
 arq = open('diversos/itens.txt', 'r', encoding='utf-8')
@@ -33,7 +33,7 @@ inventario = {0: 3, 1: 3, 11: 1, 21: 1, 31: 1}
 equipamentos = [20, 30, 11]
 
 
-# catelo -> [linha de salas][sala] | [0][0] -> "Início"
+# castelo -> [linha de salas][sala] | [0][0] -> "Início"
 castelo = (["Início", "Estábulos", "Tribunal", "Corredor Estreito", "Sala do Tesouro?"], ["Pátio", "Sala Aberta", "Armazém", "Biblioteca", "Aposentos"], ["Ponte Acidentada", "Capela", "Cozinha Real", "Sala do Trono", "Guardas Reais"], ["Jardim", "Refeitório", "QG", "Armaria", "Quarto do Rei"], ["Torre de Vigia", "Guarita", "Masmorra", "Torre do Mago", "Sala do Tesouro"])
 
 
@@ -47,9 +47,27 @@ textos_observacao[0][0] = "Você observa o início e decide rezar antes de adent
 
 
 # interacoes_desbloqueadas -> interações que podem ser realizadas | interacoes_indisponiveis -> interações que não podem ser desbloqueadas
-interacoes = {"Início": ("Rezar", "Encarar o nada")}
+interacoes = {}
 interacoes_desbloqueadas = []
-interacoes_indisponiveis = []
+interacoes_indisponiveis = ["Pegar pena", "Dar vara de pesca pro Pescador", "Reeabastecer poções nas fontes", "Liberar passagem secreta", "Atacar o Mago", "Furtar os itens do Mago", "Despetrificar estátuas", "Atacar Guardas"]
+
+arq = open('diversos/acoes.txt', 'r', encoding='utf-8')
+linhas = arq.readlines()
+
+for linha in linhas:
+    linha = linha.strip()
+
+    if (linha != "") and (len(linha.split(": ")) == 2):
+        sala, acoes = linha.split(": ")
+        acoes = acoes.split(", ")
+        interacoes[sala] = list(acoes)
+
+arq.close()
+
+""" Função para checar a variável interacoes
+for a, b in interacoes.items():
+    print(a, b)
+"""
 
 #inimigos = []
 
